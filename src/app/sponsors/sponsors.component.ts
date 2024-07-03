d52d41e52dd778a348d690e867e85cde5f9d1f33
Mostafa Mohamed <60269659+mostafaM212@users.noreply.github.com>
2024-07-03 11:29:54 +0300

    updating on ui

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -3,19 +3,26 @@ import { SponsorService } from '../services/sponsor.service';
 import { Subject, takeUntil, tap } from 'rxjs';
 import { Sponsor } from '../models/sponsor';
 import { SponsorCardComponent } from './sponsor-card/sponsor-card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-sponsors',
   standalone: true,
   providers: [SponsorService],
-  imports: [SponsorCardComponent],
+  imports: [SponsorCardComponent, MatFormFieldModule, MatInputModule, MatIconModule, NgxPaginationModule],
   templateUrl: './sponsors.component.html',
   styleUrl: './sponsors.component.scss'
 })
 export class SponsorsComponent implements OnInit, OnDestroy {
   _unsubscribe$ = new Subject<boolean>()
+  p: number = 1;
   sponsorService = inject(SponsorService)
   sponsors = signal<Sponsor[]>([])
+  searchedSponsors = signal<Sponsor[]>([])
+
   constructor() { }
 
 
@@ -30,6 +37,23 @@ export class SponsorsComponent implements OnInit, OnDestroy {
       }),
       takeUntil(this._unsubscribe$)
     ).subscribe()
+  }
+
+  onSearch(value: string) {
+    if (value.length) {
+
+      let searchedSponsor: Sponsor[] = [];
+      this.sponsors().forEach(sponsor => {
+        if (sponsor.sponsor_name.includes(value)) {
+          searchedSponsor.push(sponsor)
+        }
+      })
+      console.log('🔥', searchedSponsor);
+
+      this.searchedSponsors.set(searchedSponsor)
+    } else {
+      this.searchedSponsors.set([])
+    }
   }
   ngOnDestroy(): void {
     this._unsubscribe$.next(true)
